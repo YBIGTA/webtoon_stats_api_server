eb8b94d971726936c181c7fc1da31c77f3445283
JONGMOON\종문 <lego229@naver.com>
2016-10-25 21:53:29 +0900

    getMorphemeRank 함수에 주석 추가/ getCommentCountInFirstHour함수 및 getFirstCommentTime함수 추가 - 순록

--- sql/02stats.sql
+++ sql/02stats.sql
@@ -1,3 +1,6 @@
+# 특정 웹툰의 특정 에피소드에서 특정 형태소가 나타나는 횟수에 따라 순위를 매기는 함수
+# 1위부터 200위까지만 표시.
+# 웹툰의 stat과 자주 나타나는 형태소의 상관관계 파악을 위해 필요.
 DELIMITER //
 CREATE PROCEDURE getMorphemeRank(IN title_id INT(11), IN episode_no INT(11))
 BEGIN
@@ -34,5 +37,34 @@ BEGIN
 	FROM
 		temp_table;
 
+END //
+DELIMITER ;
+
+
+# 각 화 업데이트 이후 1시간 안에 달리는 댓글 개수
+# 현재 문제점: 웹툰 업데이트 시간이 아닌 첫댓글 시간을 기준으로 하고 있음
+#         하지만 연재를 처음 시작하는 0화, 1화 정도는 
+#         웹툰이 업데이트 되는 시점과 첫댓글 시점 간의 차이가 클 수 있음
+#         그러므로 웹툰의 업데이트 시간도 크롤링되어야 할 필요가 있음
+#         그래야 베댓 타이밍도 측정할 수 있음
+DELIMITER //
+CREATE PROCEDURE getCommentCountInFirstHour(IN title_id INT(11),IN  episode_no INT(11))
+BEGIN
+INSERT INTO comment_speed
+SELECT title_id, episode_no, count(*) as speed
+FROM comment C
+WHERE C.title_id=title_id and C.episode_no=episode_no 
+        and TIME(post_time) = TIME(SUBTIME(findFirstComment(title_id, episode_no), INTERVAL 1 HOUR));
+END //
+DELIMITER ;
+
+
+# 각 에피소드의 첫 댓글 시간 가져오기
+DELIMITER //
+CREATE PROCEDURE getFirstCommentTime(IN title_id INT(11), IN episode_no INT(11))
+BEGIN
+SELECT min(T.post_time)
+FROM comment T
+WHERE T.title_id=title_id and T.episode_no=episode_no;
 END //
 DELIMITER ;
